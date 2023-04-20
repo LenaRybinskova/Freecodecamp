4ca4383660c3a3cf88c160946d3a0eafe7a38531
«Elena <LenaRybinskova@gmail.com>
2023-04-20 18:00:37 +0300

    24

--- 1/1.js
+++ 1/1.js
@@ -13,16 +13,16 @@ let c = 4.6;
 
 // Only change code below this line
 a *= 5;
-b *=3;
+b *= 3;
 c *= 10
 
-let myStr="This is the first sentence.";
-myStr +=" ";
+let myStr = "This is the first sentence.";
+myStr += " ";
 myStr += "This is the second sentence."
 
 // Only change code below this line
 const myName = "Lena";
-const myStr = "My name is "+ myName + " and I am well!";
+const myStr = "My name is " + myName + " and I am well!";
 
 // Change code below this line
 const someAdjective = "good";
@@ -58,18 +58,17 @@ const lastName = "Lovelace";
 const thirdLetterOfLastName = lastName[2]; // Change this line
 
 
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
 
 // Setup
 const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
-
-// Setup
-const lastName = "Lovelace";
-
-// Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
 
 const myNoun = "dog";
 const myAdjective = "big";
@@ -77,30 +76,27 @@ const myVerb = "ran";
 const myAdverb = "quickly";
 
 // Only change code below this line
-const wordBlanks = "I have a " + myAdjective + " "+ myNoun +"." +"And he "+myVerb + " "+ myAdverb; // Change this line
+const wordBlanks = "I have a " + myAdjective + " " + myNoun + "." + "And he " + myVerb + " " + myAdverb; // Change this line
 // Only change code above this line
 
 
 // Only change code below this line
-const myArray = ["Lena",  33];
+const myArray = ["Lena", 33];
 
 
 // Only change code below this line
-const myArray = ["Lena", [33,23]];
-
+const myArray = ["Lena", [33, 23]];
 
 
 const myArray = [50, 60, 70];
 let myData = myArray[0];
 
 
-
 // Setup
 const myArray = [18, 64, 99];
 
 // Only change code below this line
-myArray[0]=45;
-
+myArray[0] = 45;
 
 
 const myArray = [
@@ -127,7 +123,6 @@ const myArray = [["John", 23], ["dog", 3]];
 let removedFromMyArray = myArray.shift()
 
 
-
 // Setup
 const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
@@ -136,7 +131,7 @@ myArray.shift();
 myArray.unshift(["Paul", 35])
 
 
-const myList = [["milk", 2],["bread", 1],["potatos", 5],["tomatos", 2],["chicken", 2],];
+const myList = [["milk", 2], ["bread", 1], ["potatos", 5], ["tomatos", 2], ["chicken", 2],];
 
 
 //------------------------------
@@ -144,11 +139,18 @@ const myList = [["milk", 2],["bread", 1],["potatos", 5],["tomatos", 2],["chicken
 function reusableFunction() {
     console.log("Hi World")
 }
+
 reusableFunction();
 
 
-
-function functionWithArgs(a,b) {
-    console.log(a+b)
+function functionWithArgs(a, b) {
+    console.log(a + b)
 }
-functionWithArgs(5,5)
+
+functionWithArgs(5, 5)
+
+
+function timesFive(a) {
+    return a*5
+}
+timesFive(5)
