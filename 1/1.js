097ad840cc022edf50d5e512f9d9da0aee1332b6
«Elena <LenaRybinskova@gmail.com>
2023-04-20 18:57:26 +0300

    30

--- 1/1.js
+++ 1/1.js
@@ -237,3 +237,17 @@ function welcomeToBooleans() {
 
     // Only change code above this line
 }
+
+
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if(wasThatTrue){
+        return "Yes, that was true"
+    }
+    return "No, that was false"
+
+
+    // Only change code above this line
+
+}
