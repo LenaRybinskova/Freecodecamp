80e990425338b59957208b09b9fd2b64a200461c
«Elena <LenaRybinskova@gmail.com>
2023-04-20 17:48:20 +0300

    20

--- 1/1.js
+++ 1/1.js
@@ -125,3 +125,12 @@ const myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line
 let removedFromMyArray = myArray.shift()
+
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35])
