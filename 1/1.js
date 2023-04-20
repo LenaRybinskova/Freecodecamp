64c02d8c2a45fa4f0dcee15e01a22a9768c3607f
«Elena <LenaRybinskova@gmail.com>
2023-04-20 17:41:23 +0300

    18

--- 1/1.js
+++ 1/1.js
@@ -111,3 +111,10 @@ const myArray = [
 ];
 
 const myData = myArray[2][1];
+
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
