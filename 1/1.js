475b06ff3a8bc329be2b7456bd6cdef374c68e75
«Elena <LenaRybinskova@gmail.com>
2023-05-09 18:36:46 +0300

    47

--- 1/1.js
+++ 1/1.js
@@ -296,3 +296,16 @@ function testStrictNotEqual(val) {
     return "Equal";
   }
 //   console.log(testStrictNotEqual("bob"))
+
+function testGreaterThan(val) {
+    if (va>100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  } 
+//   testGreaterThan(10);
