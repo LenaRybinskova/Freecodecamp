94c1b4b2a0fa5bba72a8ed09d530f28ce26e24d9
«Elena <LenaRybinskova@gmail.com>
2023-05-09 19:22:59 +0300

    58

--- 1/1.js
+++ 1/1.js
@@ -429,3 +429,28 @@ else if(strokes===(par + 3) || strokes>(par + 3)) return "Go Home!"
   // Only change code above this line
 }
 // console.log(golfScore(5, 4))
+
+
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch(val){
+    case 1:
+        answer="alpha"
+        break; 
+    case 2:
+        answer="beta"
+        break;
+    case 3:
+        answer="gamma"
+        break;        
+    case 4:
+        answer="delta"
+        break;              
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  caseInSwitch(1);
