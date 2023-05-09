b5f83e0871578a7985235a05182dd93f8fef2291
«Elena <LenaRybinskova@gmail.com>
2023-05-09 18:38:53 +0300

    49

--- 1/1.js
+++ 1/1.js
@@ -322,3 +322,16 @@ function testGreaterOrEqual(val) {
     return "Less than 10";
   }
 //   testGreaterOrEqual(10);
+
+function testLessThan(val) {
+    if (val<25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val<55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+//   testLessThan(10);
