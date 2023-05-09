2208dc5e9984a2113d8ad49e2ff2d57dec85974e
«Elena <LenaRybinskova@gmail.com>
2023-05-09 18:29:25 +0300

    46

--- 1/1.js
+++ 1/1.js
@@ -278,3 +278,13 @@ function compareEquality(a, b) {
     return "Not Equal";
   } 
 //   compareEquality(10, "10");
+
+// Setup
+function testNotEqual(val) {
+    if (val!=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
