e29bf31333ced9136f3d04a575a51b6dadcf492f
«Elena <LenaRybinskova@gmail.com>
2023-05-09 18:58:50 +0300

    55

--- 1/1.js
+++ 1/1.js
@@ -388,3 +388,15 @@ function testElseIf(val) {
     return "Between 5 and 10"};
   }
 //   testElseIf(7);
+
+
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+//   orderMyLogic(7);
