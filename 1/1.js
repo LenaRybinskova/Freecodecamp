e88d0710a2196afa74cd73a7adc643266fb65d78
«Elena <LenaRybinskova@gmail.com>
2023-05-09 19:16:16 +0300

    57

--- 1/1.js
+++ 1/1.js
@@ -413,3 +413,19 @@ function testSize(num) {
     // Only change code above this line
   } 
 // console.log(testSize(3))
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if(strokes===1) return "Hole-in-one!"
+else if(strokes===(par - 2) || strokes<(par - 2)) return "Eagle"
+else if(strokes===(par - 1)) return "Birdie"
+else if(strokes===par) return "Par"
+else if(strokes===(par + 1)) return "Bogey"
+else if(strokes===(par + 2)) return "Double Bogey"
+else if(strokes===(par + 3) || strokes>(par + 3)) return "Go Home!"
+  return "Change Me";
+  // Only change code above this line
+}
+// console.log(golfScore(5, 4))
