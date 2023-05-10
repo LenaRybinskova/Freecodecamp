cad43026148a48aefd84455b848b353a98042b47
«Elena <LenaRybinskova@gmail.com>
2023-05-10 18:06:54 +0300

    70

--- 1/1.js
+++ 1/1.js
@@ -602,3 +602,21 @@ const myDog5 = {
 
 // Only change code below this line
 delete myDog5.tails
+
+
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+  const lookup ={
+    alpha:"Adams",
+    bravo:"Boston",
+    charlie:"Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank"
+  }
+  return lookup[val]
+
+  return result;
+}
+// phoneticLookup("charlie");
