4402d507f519246b3a836ac4beddd3e8592ecb6e
«Elena <LenaRybinskova@gmail.com>
2023-05-10 17:39:54 +0300

    67

--- 1/1.js
+++ 1/1.js
@@ -572,3 +572,13 @@ const testObj3 = {
 };
 const playerNumber = 16;  // Change this line
 const player = testObj3[playerNumber];   // Change this line
+
+
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name="Happy Coder"
