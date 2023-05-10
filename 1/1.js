4cc28d985d06c7fdad167e7b03a631b3a95b79ba
«Elena <LenaRybinskova@gmail.com>
2023-05-10 17:22:38 +0300

    62

--- 1/1.js
+++ 1/1.js
@@ -528,3 +528,12 @@ function isLess(a, b) {
 return a < b
 }
 // isLess(10, 15);
+
+// Setup
+function abTest(a, b) {
+if(a<0 || b<0){
+  return undefined
+}
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+// console.log(abTest(9,9))
