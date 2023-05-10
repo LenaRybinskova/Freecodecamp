0147c5f06f87ccec45b56f68b1f1fcc053a1ca26
«Elena <LenaRybinskova@gmail.com>
2023-05-10 18:12:33 +0300

    71

--- 1/1.js
+++ 1/1.js
@@ -620,3 +620,9 @@ function phoneticLookup(val) {
   return result;
 }
 // phoneticLookup("charlie");
+
+
+function checkObj(obj, checkProp) {
+return obj.hasOwnProperty(checkProp)? obj[checkProp] :"Not Found"
+}
+// console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift"))
