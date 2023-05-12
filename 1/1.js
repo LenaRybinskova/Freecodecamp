14155bebe2ab6b71a224dc9cd5844f9ab2514d96
«Elena <LenaRybinskova@gmail.com>
2023-05-12 15:42:27 +0300

    50

--- 1/1.js
+++ 1/1.js
@@ -687,5 +687,79 @@ const myPlants = [
     ]
   }
 ];
-
 const secondTree = myPlants[1].list[1];
+
+
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+// -----------НЕ РЕШЕНО
+function updateRecords(records, id, prop, value) {
+    if(value===""){
+      let newArr={...records}
+      delete newArr[id][prop]
+      return newArr
+    }
+    if(prop !="tracks" && value!=""){
+      let newArr={...records}
+      newArr[id][prop]=value
+      return newArr
+    }
+    if(prop ==="tracks" && value!=""){
+      if(!records[id].hasOwnProperty([prop])){
+        let newArr={...records}
+        newArr[id][prop] = []
+        newArr[id][prop]=value
+        return newArr
+      }
+    }
+  
+
+      
+
+    }
+
+// 
+// console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"))
+
+// -----------как то работала
+// if(value==="" && records[id][prop] != 'tracks'){
+//   let newArr={...records}
+//   delete newArr[id][prop]
+//   return newArr
+// } else if(records[id].hasOwnProperty([prop])){
+//   let newArr={...records}
+//   newArr[id][prop].push(value)
+//   return newArr
+// } else {
+//   let newArr={...records}
+//   newArr[id][prop] = []
+//   newArr[id][prop]=value
+//   return newArr
+// }
+
+// Setup
+const myArray5 = [];
+let i=5
+while(i>=0){
+  myArray5.push(i)
+  i--
+}
+// console.log(myArray5)
+
