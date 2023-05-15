c8827db9756248a8503abecc0e41ab410238fe84
«Elena <LenaRybinskova@gmail.com>
2023-05-15 21:39:36 +0300

    58

--- 1/1.js
+++ 1/1.js
@@ -808,13 +808,74 @@ do{
 }
 while (i < 5) 
 
-function sum(arr, n) {
+// function sum(arr, n) {
+//   // Only change code below this line
+//   if(n <= 0) {
+//     return 0;
+//   } else {
+//     return sum(arr, n - 1) + arr[n - 1];
+//   }
+//   // Only change code above this line
+// }
+// console.log(sum([2, 3, 4], 1))
+
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+//                  "Bob", "potato"
+//                  "Kristian", "lastName"
+function lookUpProfile(name, prop) {
+  
+}
+// console.log(lookUpProfile("Bob", "number"))
+
+
+// function lookUpProfile(name, prop) {
+//   
+//   for( let i=0; i<contacts.length;i++){
+//     if( contacts[i].hasOwnProperty([prop]) ){
+//       if(name==contacts[i].firstName){     
+//         return contacts[i][prop]
+//       } 
+// 
+//     }else{
+//         return "No such property"
+//       }
+//     }
+//   }
+// 
+// console.log(lookUpProfile("Kristian", "lastName"))
+
+
+function randomFraction() {
+
   // Only change code below this line
-  if(n <= 0) {
-    return 0;
-  } else {
-    return sum(arr, n - 1) + arr[n - 1];
-  }
+
+  return Math.random()
+
   // Only change code above this line
 }
-// console.log(sum([2, 3, 4], 1))
